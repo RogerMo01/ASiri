6c1939718c09c522fa99af51933e95cb1f5caf12
Roger <morenoroger001@gmail.com>
2024-07-01 11:15:42 -0400

    feat: Add base code for LLM

---
+++ src/gemini.py
@@ -0,0 +1,40 @@
+import os
+from typing import Any
+from dotenv import load_dotenv
+import google.generativeai as genai
+from google.generativeai.types import *
+from llm import LLM
+
+
+class Gemini(LLM):
+    def __init__(self) -> None:        
+        # Load API KEY variable from .env file
+        load_dotenv()
+        key = os.getenv("API_KEY")
+        genai.configure(api_key=key)
+
+        self.model = genai.GenerativeModel('gemini-pro')
+
+
+    def __call__(self, query: str, temperature=1.0) -> str:
+        safety_settings={
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+        }
+        
+        response = self.model.generate_content(
+            query, 
+            generation_config=genai.types.GenerationConfig(temperature=temperature),
+            safety_settings=safety_settings
+        )
+        
+        return response.text 
+
+
+model = Gemini()
+while True:
+    message = input("input: ")
+    response = model(message)
+    print(response)
---
+++ src/llm.py
@@ -0,0 +1,8 @@
+from typing import Any
+
+class LLM:
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self) -> Any:
+        raise NotImplemented('Not implemented in LLM abstract class')
