ec16df317beefee75b23d9a7609bd07956b5dae4
Roger <morenoroger001@gmail.com>
2024-07-22 16:04:19 -0400

    fix: Make request endpoint dynamic

--- gui/src/components/axios_aux.ts
+++ gui/src/components/axios_aux.ts
@@ -4,8 +4,8 @@ const serverIP = import.meta.env.VITE_SERVER_IP;
 const serverPORT = import.meta.env.VITE_SERVER_PORT;
 console.log(`Remote server address: ${serverIP}:${serverPORT}`)
 
-export function request(setter: React.Dispatch<React.SetStateAction<never[]>>) {
-    axios.get(`http://${serverIP}:${serverPORT}/api/names`)
+export function request(endpoint: string, setter: React.Dispatch<React.SetStateAction<never[]>>) {
+    axios.get(`http://${serverIP}:${serverPORT}${endpoint}`)
     .then((response) => {
         setter(response.data.names);
     })
@@ -16,5 +16,8 @@ export function request(setter: React.Dispatch<React.SetStateAction<never[]>>) {
 
 export function repost(endpoint: string, data: unknown, headers: AxiosHeaders){
     const serverUrl = `http://${serverIP}:${serverPORT}${endpoint}`;
-    axios.post(serverUrl, data, {headers: headers});
+    axios.post(serverUrl, data, {headers: headers})
+    .catch((error) => {
+        console.error(error);
+    });
 }
